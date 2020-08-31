e40a456b437748a0043d43a06c1fcb9b960491a1
kazuki13070311 <kazuki13070311@gmail.com>
2020-08-31 22:17:41 +0900

    📖Udemy学習(Vue.jsセクション7-6まで学習完了)

--- test-vuejs/src/components/LikeNumber.vue
+++ test-vuejs/src/components/LikeNumber.vue
@@ -1,15 +1,25 @@
 <template>
     <div>
-        <p>いいね({{ number }})</p>
+        <p>いいね({{ halfNumber }})</p>
         <button @click="increment">+1</button>
     </div>
 </template>
 
 <script>
 export default {
-  data(){
-      return {
-          number: 5
+  props: {
+      totalNumber: {
+          type: Number,
+          default: function(){
+              return{
+                  number: 5
+              }
+          }
+      }
+  },
+  computed: {
+      halfNumber(){
+          return this.totalNumber / 2;
       }
   },
   methods: {
