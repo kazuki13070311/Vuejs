b9b17c75ecbbfec3036d7475ede2b2e33fe0c0a5
kazuki13070311 <kazuki13070311@gmail.com>
2020-09-09 20:29:16 +0900

    📖Udemy学習(セクション12-10まで完了)

--- test-vuejs/src/App.vue
+++ test-vuejs/src/App.vue
@@ -4,6 +4,12 @@
     <transition name="fade">
       <p v-if="show">hello</p>
     </transition>
+    <transition 
+      name="slide" 
+      type="animation"
+    >
+      <p v-show="show">bye</p>
+    </transition>
   </div>
 </template>
 
@@ -19,22 +25,45 @@ export default {
 
 <style scoped>
 .fade-enter {
-
+  opacity: 0;
 }
 .fade-enter-active {
-
+  transition: opacity 0.5s;
 }
 .fade-enter-to {
-
+  opacity: 1;
 }
 .fade-leave {
-
+  opacity: 1;
 }
 .fade-leave-active {
-
+  transition: opacity 0.5s;
 }
 .fade-leave-to {
+  opacity: 0;
+}
 
+
+.slide-enter,
+.slide-leave-to {
+  opacity: 0;
+}
+.slide-enter-active {
+  animation: slide-in 0.5s;
+  transition: opacity 1s;
+}
+.slide-leave-active {
+  animation: slide-in 0.5s reverse;
+  transition: opacity 1s;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateX(100px);
+  }
+  to {
+    transform: translateX(0);
+  }
 }
 
 .main {
