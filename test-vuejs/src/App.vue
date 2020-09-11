299c9022a5d140772877d51b7faaee540208e8e1
kazuki13070311 <kazuki13070311@gmail.com>
2020-09-11 21:28:43 +0900

    📖Udemy学習(Vue.jsセクション12-25まで完了)

--- test-vuejs/src/App.vue
+++ test-vuejs/src/App.vue
@@ -3,18 +3,26 @@
     <button @click="myAnimation = 'slide'">Slide</button>
     <button @click="myAnimation = 'Fade'">Fade</button>
     <p>{{ myAnimation }}</p>
+    <br>
+    <button @click="add">追加</button>
+    <ul style="width: 200px; margin: auto;">
+      <transition-group name="fade" tag="div">
+        <li 
+          style="cursor: pointer; "
+          v-for="(number,index) in numbers"
+          :key="number"
+          @click="remove(index)"
+        >{{ number }}</li>
+      </transition-group>
+    </ul>
+    <br>
     <button @click="show = !show">切り替え</button>
     <br><br>
     <transition
+      :css="false"
       @before-enter="beforeEnter"
       @enter="enter"
-      @after-enter="afterEnter"
-      @enter-cancelled="enterCancelled"
-
-      @before-leave="beforeLeave"
       @leave="leave"
-      @after-leave="afterLeave"
-      @leave-cancelled="leaveCancelled"
     >
       <div class="circle" v-if="show"></div>
     </transition>
@@ -55,34 +63,51 @@ export default {
   },
   data(){
     return {
+      numbers: [0,1,2],
+      nextNumber: 3,
       show: true,
       myAnimation: "slide",
       myComponent: "ComponentA"
     };
   },
   methods: {
+    randomIndex(){
+      return Math.floor(Math.random() * this.numbers.length);
+    },
+    add(){
+      this.numbers.splice(this.randomIndex(), 0, this.nextNumber);
+      this.nextNumber += 1;
+    },
+    remove(index){
+      this.numbers.splice(index, 1);
+    },
     beforeEnter(el){
-    
+      el.style.transform = 'scale(0)'
     },
     enter(el, done){
-
+      let scale = 0;
+      const interval = setInterval(() => {
+        el.style.transform = `scale(${scale})`;
+        scale += 0.1
+        if (scale > 1 ) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
     },
-    afterEnter(el){
 
-    },
-    enterCancelled(el){
-
-    },
-    beforeLeave(el){
-
-    },
     leave(el, done){
-
+      let scale = 1;
+      const interval = setInterval(() => {
+        el.style.transform = `scale(${scale})`;
+        scale -= 0.1
+        if (scale < 0 ) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
     },
-    afterLeave(el){
-
-    },
-    leaveCancelled(el){
+    leaveCancelled(){
 
     },
   }
@@ -98,6 +123,9 @@ export default {
   background-color: deeppink;
 }
 
+.fade-move {
+  transition: transform 1s;
+}
 .fade-enter {
   opacity: 0;
 }
@@ -112,6 +140,8 @@ export default {
 }
 .fade-leave-active {
   transition: opacity 0.5s;
+  position: absolute;
+  width: 200px;
 }
 .fade-leave-to {
   opacity: 0;
