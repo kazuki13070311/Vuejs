aa6eb6e75a6e4a8c3874c615f15a83ce2d06ffba
kazuki13070311 <kazuki13070311@gmail.com>
2020-09-12 11:04:16 +0900

    📖Udemy学習(Vue.jsセクション13-16まで完了)

--- test-vuejs/src/views/Users.vue
+++ test-vuejs/src/views/Users.vue
@@ -7,7 +7,7 @@
         <h1>User No. {{ id }}</h1>
         <router-link :to="'/users/' + (Number(id) + 1) + '/profile'">次のユーザー</router-link>
         <br>
-        <router-link :to="{ name: 'users-id-profile', params: { id:  Number(id) + 1} }">次のユーザー</router-link>
+        <router-link :to="{ name: 'users-id-profile', params: { id:  Number(id) + 1},query: { lang: 'ja', page: 2} }">次のユーザー</router-link>
         <router-view></router-view>
     </div>
 </template>
