75c0857adc9c694e23347c555b044688d133bb94
kazuki13070311 <kazuki13070311@gmail.com>
2020-09-13 14:26:02 +0900

    📖Udemy学習(セクション13-27まで学習完了)

--- test-vuejs/src/router.js
+++ test-vuejs/src/router.js
@@ -1,11 +1,20 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from "./views/Home.vue";
-import Users from "./views/Users.vue";
-import UsersPosts from "./views/UsersPosts.vue";
-import UsersProfile from "./views/UsersProfile.vue";
-import HeaderHome from "./views/HeaderHome.vue";
-import HeaderUsers from "./views/HeaderUsers.vue";
+
+//↓この書き方にすれば遅延を防いでパフォーマンスをあげれる
+const Home = () => import("./views/Home.vue");
+const Users = () => import("./views/Users.vue");
+const UsersPosts = () => import("./views/UsersPosts.vue");
+const UsersProfile = () => import("./views/UsersProfile.vue");
+const HeaderHome = () => import("./views/HeaderHome.vue");
+const HeaderUsers = () => import("./views/HeaderUsers.vue");
+
+//import Home from "./views/Home.vue";
+//import Users from "./views/Users.vue";
+//import UsersPosts from "./views/UsersPosts.vue";
+//import UsersProfile from "./views/UsersProfile.vue";
+//import HeaderHome from "./views/HeaderHome.vue";
+//import HeaderUsers from "./views/HeaderUsers.vue";
 
 Vue.use(Router)
 
@@ -17,7 +26,7 @@ export default new Router({
             components: {
                 default: Home,
                 header: HeaderHome
-            }
+            },
         },
         { 
             path: "/users/:id", 
@@ -25,11 +34,34 @@ export default new Router({
                 default: Users,
                 header: HeaderUsers
             },
-            props: true, 
+            props: {
+                default: true,
+                header: false
+            } ,
             children: [
             { path: "posts", component: UsersPosts },
             { path: "profile", component: UsersProfile, name: "users-id-profile" }
             ]
+        },{
+            path: "*",
+            redirect: "/"
         }
-    ]
+    ],
+    scrollBehavior(to, savedPosition) {
+        return new Promise(resolve => {
+            this.app.$root.$once('triggerScroll', () => {
+                let position = {x: 0, y:0};
+                if(savedPosition) {
+                    position = savedPosition;
+                }
+                if(to.hash){
+                    position = {
+                        selector: to.hash,
+                        offset: {x:0, y:50}
+                    };
+                }
+                resolve(position);
+            });
+        });
+    }
 });
